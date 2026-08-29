4f21b5f2bf3efafbceaa3d647ed8c37f5bb74d09
fidel <dogblueten@gmail.com>
2026-08-29 18:38:31 -0400

    class 3

---
+++ src/components/TheContador.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Hello Lola</h1>
+  <p class="good">{{contador}}</p>
+  <button @click="incrementar">Incrementar</button>
+  <button @click="reducir">Reducir</button>
+  <p class="bad">{{respuesta}}</p>
+
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+  const contador = ref(0);
+  const respuesta = ref('');
+  const incrementar = () =>{
+    respuesta.value='';
+    contador.value++;
+  }
+  const reducir = () =>{
+    respuesta.value='';
+    if(contador.value==0 || contador.value<0){
+      respuesta.value = 'no puedes bajar de 0'
+    }else{
+      contador.value--;
+    }   
+  }
+</script>
+
+<style scoped>
+  .good{
+    color: green;
+  }
+  .bad{
+    color: red;
+  }
+</style>
