42b043f18894061a1417c982a19f73ac2ba998f2
fidel <dogblueten@gmail.com>
2026-08-29 21:54:40 -0400

    class 7

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="sidebar">
-        <h2>Barra Lateral</h2>
+        <h2>Bienvenido</h2>
+        <h3>Nombre: {{ nombre }}</h3>
+        <h3>Correo: {{ email }}</h3>
         <ul>
             <li><router-link to="/">Inicio</router-link></li>
             <li><router-link to="/contador">Contador</router-link></li>
@@ -9,7 +11,20 @@
         </ul>
     </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import { useRegistrarStore } from "@/modules/registro/stores/registrarStore";
+    import {ref,watch} from "vue";
+
+    const registrarStore = useRegistrarStore();
+    const nombre = ref(registrarStore.nombre);
+    const email = ref(registrarStore.email);
+    watch(() => registrarStore.nombre, (newNombre) => {
+        nombre.value = newNombre;
+    });
+    watch(() => registrarStore.email, (newEmail) => {
+        email.value = newEmail;
+    });
+</script>
 <style scoped>
     .sidebar{
         width: 200px;
--- src/modules/registro/views/RegistroView.vue
+++ src/modules/registro/views/RegistroView.vue
@@ -3,10 +3,10 @@
         <h2>Formulario de registro</h2>
         <Form :validation-schema="schema" @submit="onSubmit">
             <Label for="nombre">Nombre:</Label>
-            <Field name="nombre" type="text" placeholder="ingrese su nombre" id="nombre"/>
+            <Field v-model="nombre" name="nombre" type="text" placeholder="ingrese su nombre" id="nombre"/>
             <ErrorMessage name="nombre"></ErrorMessage>
             <Label for="email">Correo:</Label>
-            <Field name="email" type="email" placeholder="ingrese su email" id="email" />
+            <Field v-model="email" name="email" type="email" placeholder="ingrese su email" id="email" />
             <ErrorMessage name="email"></ErrorMessage>
             <button type="submit">Registrar</button>
         </Form>
@@ -16,9 +16,17 @@
 <script setup lang="ts">
     import { Form,Field,ErrorMessage } from "vee-validate";
     import {schema} from '../schemas/validationSchema'
+    import { useRegistrarStore } from "../stores/registrarStore";
+    import { ref } from "vue";
+
+    const registrarStore = useRegistrarStore();
+
+    const nombre = ref('');
+    const email = ref('');
 
     const onSubmit = () =>{
-        alert('Se ha enviado el formulario')
+        registrarStore.guardarRegistro(nombre.value, email.value);
+        alert('Registro guardado correctamente');
     }
 </script>
 <style scoped>
