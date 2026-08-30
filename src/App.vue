a2f88568ec64a09fba8f8aface059eee2a6f3e3c
fidel <dogblueten@gmail.com>
2026-08-29 20:15:15 -0400

    class 4

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <TheContador></TheContador>
+  <TaskList></TaskList>
 </template>
 
 <script setup lang="ts">
   import TheContador from "./components/TheContador.vue";
+  import TaskList from "./components/TaskList.vue";
 </script>
 
 <style scoped>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>Lista de TAreas</h2>
+        <input type="text" v-model="nuevaTarea" @keyup.enter="addTarea">
+        <ul>
+            <li v-for="(tarea, index) in tareas" :key="index">
+                {{tarea}}
+                <button @click="deleteTarea(index)">Eliminar Tarea</button>
+            </li>
+        </ul>
+        <button @click="deleteTodo">Eliminar</button>
+    </div>
+</template>
+<script setup lang="ts">
+    import { ref } from 'vue';
+    const tareas = ref<string[]>([]);
+    const nuevaTarea = ref('');
+
+    const addTarea = () =>{
+        tareas.value.push(nuevaTarea.value)
+        nuevaTarea.value = '';
+    }
+    const deleteTodo = () => {
+        tareas.value = [];
+    }
+    const deleteTarea = (index: number) => {
+        tareas.value.splice(index, 1);
+    }
+</script>
+<style>
+</style>
