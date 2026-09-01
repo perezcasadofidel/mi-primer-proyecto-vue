36b8e65fbd82a1af651e40e7855ba4c88cd326b3
fidel <dogblueten@gmail.com>
2026-09-01 12:29:52 -0400

    class 8 completed

--- src/helpers/getWeather.ts
+++ src/helpers/getWeather.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'ejemplo.com'
+const API_URL = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&timezone=auto'
 
 export const getTemperatura = async () => {
     const respuesta = await axios.get(API_URL);
